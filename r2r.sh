02cc128156c6432970139d32d8a7ed60b4bad7e5
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-06-20 22:36:51 +0530

    Update r2r.sh

--- r2r.sh
+++ r2r.sh
@@ -121,3 +121,4 @@ echo -e "Installing Fast web fuzzer (ffuf) "
 go get -u github.com/ffuf/ffuf
 sudo cp $HOME/go/bin/ffuf /usr/local/bin
 echo -e "Finished installing Fast web fuzzer (ffuf)\n"
+echo "you are Ready-to-Recon"
